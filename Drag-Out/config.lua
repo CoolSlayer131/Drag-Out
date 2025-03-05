6c577e2408b2904a99a22e1639aa8341a234c02d
CoolSlayer131 <61106540+CoolSlayer131@users.noreply.github.com>
2025-03-05 16:55:03 -0600

    Update config.lua
    Added options to use okoknotify

--- Drag-Out/config.lua
+++ Drag-Out/config.lua
@@ -2,8 +2,12 @@ Config = {
     debug = true,
 
     ClockedOn = "dragout.leo", -- Ace permission required to break windows
+
+    useNotify = true, -- Use okokNotify instead of chat messages
+    notifaudio = true, -- Play a sound with the notification (only works if useNotify is true)
 }
 
 Config.weapons = {
-    {"WEAPON_HAMMER", "WEAPON_NIGHTSTICK", "WEAPON_CROWBAR", "WEAPON_BAT", "WEAPON_GOLFCLUB", "WEAPON_WRENCH", "WEAPON_FLASHLIGHT"},
+    "WEAPON_HAMMER", "WEAPON_NIGHTSTICK", "WEAPON_CROWBAR",
+    "WEAPON_BAT", "WEAPON_GOLFCLUB", "WEAPON_WRENCH", "WEAPON_FLASHLIGHT"
 }
