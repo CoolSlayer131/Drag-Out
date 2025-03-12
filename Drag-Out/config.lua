88205dc6a150db310bec56a57a070034892d69eb
CoolSlayer131 <61106540+CoolSlayer131@users.noreply.github.com>
2025-03-11 19:01:52 -0500

    Update config.lua
    Updated Blacklist function
    Added okokNotify Settings
    uses weapon config?

--- Drag-Out/config.lua
+++ Drag-Out/config.lua
@@ -1,13 +1,20 @@
 Config = {
-    debug = true,
+    debug = false,
 
-    ClockedOn = "dragout.leo", -- Ace permission required to break windows
-
-    useNotify = true, -- Use okokNotify instead of chat messages
-    notifaudio = true, -- Play a sound with the notification (only works if useNotify is true)
+    usePerms = false,
+    ClockedOn = "dragout.leo", -- Ace permission required to break windows (If UsePerms is set to true)
 }
 
 Config.weapons = {
     "WEAPON_HAMMER", "WEAPON_NIGHTSTICK", "WEAPON_CROWBAR",
     "WEAPON_BAT", "WEAPON_GOLFCLUB", "WEAPON_WRENCH", "WEAPON_FLASHLIGHT"
 }
+
+Config.useNotify = true -- Use okokNotify instead of chat messages
+Config.notifaudio = true -- Play a sound with the notification (only works if useNotify is true)
+
+Config.VehicleBlacklist = { --Use Spawncodes
+    "example_vehicle1",
+    "example_vehicle2",
+}
+
